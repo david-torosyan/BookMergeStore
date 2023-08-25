335041a8ae0af509a86a6053a4b2c80d1c294267
Torosyan David <davit.torosyan.0214@gmail.com>
2023-08-26 02:41:22 +0400

    Add project files.

---
+++ BookMergeStore/Controllers/CategoryController.cs
@@ -0,0 +1,80 @@
+﻿using BookMergeStore.Data;
+using BookMergeStore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookMergeStore.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            List<Category> model = _db.Categories.ToList();
+
+            return View(model);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+
+        }
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id < 0)
+            {
+                return NotFound();
+            }
+
+            Category? category = _db.Categories.FirstOrDefault(c => c.Id == id);
+
+            if(category == null) 
+            {
+                return NotFound();
+            }
+
+            List<Category> model = _db.Categories.ToList();
+            ViewData["id"] = id;
+                
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {      
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+
+        }
+
+    }
+}
---
+++ BookMergeStore/Models/Category.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookMergeStore.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        [DisplayName("Category Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [Range(0, 100)]
+        [DisplayName("Dispaly Order")]
+        public int DisplayOrder { get; set; }
+
+    }
+}
